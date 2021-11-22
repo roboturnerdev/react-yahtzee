64bcd25a7bd5bfbc3a4cd4368df22a3b1db4d5af
Robert Turner <roboturnerdev@gmail.com>
2021-11-22 12:02:15 -0500

    converted numbers for dice into actual icons, displayed appropriately, and also styled.

--- src/Die.js
+++ src/Die.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numberWords: ['one', 'two', 'three', 'four', 'five', 'six']
+  };
   constructor(props) {
     super(props);
-
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
@@ -12,14 +14,17 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
+    const { numberWords, locked, val, disabled } = this.props;
+
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+    if(locked) classes += 'Die-locked';
+
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+      <i
+        className={classes}
         onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
+        disabled={disabled}
+      />
     );
   }
 }
